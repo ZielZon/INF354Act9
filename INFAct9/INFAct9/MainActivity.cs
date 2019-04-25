ec7638bea29db2bd3b3b1819af4e727131953f76
ZielZon <u16182732@tuks.co.za>
2019-04-25 09:57:46 +0200

    Commiting Assignment

---
+++ INFAct9/INFAct9/MainActivity.cs
@@ -0,0 +1,106 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Content;
+using System;
+using System.Collections.Generic;
+using Android.Runtime;
+
+namespace INFAct9
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        /*  protected override void OnCreate(Bundle savedInstanceState)
+          {
+              base.OnCreate(savedInstanceState);
+              Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+              // Set our view from the "main" layout resource
+              SetContentView(Resource.Layout.activity_main);
+          }*/
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        EditText edtTask;
+        sqldbClass sqldb;
+        adapter adapter;
+        ListView lstTask;
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_add:
+                    edtTask = new EditText(this);
+                    Android.Support.V7.App.AlertDialog alertDialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("Add New Shopping List Item")
+                        .SetMessage("Item?")
+                        .SetView(edtTask)
+                        .SetPositiveButton("Add", OkAction)
+                        .SetNegativeButton("Cancel", CancelAction)
+                        .Create();
+                    alertDialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+        private void ButtonAddClicked (object sender, EventArgs e)
+        {
+
+            
+                edtTask = new EditText(this);
+                Android.Support.V7.App.AlertDialog alertDialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                    .SetTitle("Add New Shopping List Item")
+                    .SetMessage("Item?")
+                    .SetView(edtTask)
+                    .SetPositiveButton("Add", OkAction)
+                    .SetNegativeButton("Cancel", CancelAction)
+                    .Create();
+                alertDialog.Show();
+          
+                    
+        
+       
+        }
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+        }
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = edtTask.Text;
+            sqldb.InsertNewTask(task);
+            LoadTaskList();
+        }
+        public void LoadTaskList()
+        {
+            List<string> taskList = sqldb.getTaskList();
+            adapter = new adapter(this, taskList, sqldb);
+            lstTask.Adapter = adapter;
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.activity_main);
+
+            sqldb = new sqldbClass(this);
+            lstTask = FindViewById<ListView>(Resource.Id.lstTask);
+            Button btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            btnAdd.Click += ButtonAddClicked;
+            
+            //Load Data  
+            LoadTaskList();
+        }
+    }
+}
---
+++ INFAct9/INFAct9/adapter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace INFAct9
+{
+    class adapter : BaseAdapter
+    {
+        private MainActivity mainActivity;
+        private List<string> taskList;
+        private sqldbClass dbHelper;
+
+        public adapter(MainActivity mainActivity, List<string> taskList, sqldbClass dbHelper)
+        {
+            this.mainActivity = mainActivity;
+            this.taskList = taskList;
+            this.dbHelper = dbHelper;
+        }
+        public override int Count { get { return taskList.Count; } }
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            LayoutInflater inflater = (LayoutInflater)mainActivity.GetSystemService(Context.LayoutInflaterService);
+            View view = inflater.Inflate(Resource.Layout.row, null);
+            TextView txtTask = view.FindViewById<TextView>(Resource.Id.task_title);
+            Button btnDelete = view.FindViewById<Button>(Resource.Id.btnDelete);
+            Button btnTaskTitle = view.FindViewById<Button>(Resource.Id.task_title);
+          
+            txtTask.Text = taskList[position];
+           
+            btnTaskTitle.LongClick += delegate
+            {
+                string task = taskList[position];
+                dbHelper.deleteTask(task);
+                mainActivity.LoadTaskList(); // Reload Data 
+            };
+            btnDelete.Click += delegate
+            {
+                string task = taskList[position];
+                dbHelper.deleteTask(task);
+                mainActivity.LoadTaskList(); // Reload Data  
+            };
+            return view;
+        }
+
+       
+    }
+}
